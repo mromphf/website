e1db9ac22864a01c8ed20b30d97c92e439517806
Mark Romphf <markromphf@gmail.com>
2025-04-09 20:06:56 -0600

    First pass of the Masthead component.

---
+++ src/components/Masthead.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: "Masthead"
+}
+</script>
+
+<template>
+  <header>
+    <div class="header-left">
+      <img src="/public/Teddy2.jpg" alt="Mike Romeo" height="200" width="200"/>
+    </div>
+    <div class="header-right">
+      <h2>Mark</h2>
+      <h1>"Mike Romeo"</h1>
+      <h2>Romphf</h2>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: beige;
+  width: 100%;
+  height: 18vh;
+  margin-top: 5vh;
+}
+
+header h1, h2 {
+  text-align: center;
+  line-height: 2.5rem;
+}
+
+header .header-left, .header-right {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 48%;
+}
+
+header .header-right {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+header img {
+  max-width: 100%;
+  height: auto;
+}
+
+</style>
