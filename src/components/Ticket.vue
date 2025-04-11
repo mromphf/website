2dabff24714c7f9e1c7fb7b82436a0886602b725
Mark Romphf <markromphf@gmail.com>
2025-04-10 19:34:07 -0600

    Introduce Ticket component.

---
+++ src/components/Ticket.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "Ticket"
+}
+</script>
+
+<template>
+  <article>
+    <h2>Ticket Please</h2>
+  </article>
+</template>
+
+<style scoped>
+article {
+  background-color: aquamarine;
+  width: 48%;
+  margin: 1%;
+  padding: 1%;
+}
+
+article h2 {
+  text-align: center;
+}
+</style>
