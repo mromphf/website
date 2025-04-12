820cbdcd88e4c6a77a8685f308f0191d8711e6b5
Mark Romphf <markromphf@gmail.com>
2025-04-12 11:01:05 -0600

    Style out the two columns of tickets.

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -9,13 +9,24 @@ export default {
 
 <template>
   <Masthead />
-  <Ticket/>
-  <Ticket/>
-  <Ticket/>
-  <Ticket/>
-  <Ticket/>
-  <Ticket/>
+  <section id="ticketHousing">
+    <Ticket/>
+    <Ticket/>
+    <Ticket/>
+    <Ticket/>
+    <Ticket/>
+    <Ticket/>
+  </section>
 </template>
 
 <style scoped>
+#ticketHousing {
+  border: 1px red solid;
+  column-count: 2;
+  column-gap: 1rem;
+  margin-top: 2.5rem;
+  max-height: 80vh;
+  overflow-y: auto;
+  padding: 1rem;
+}
 </style>
--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -12,13 +12,16 @@ export default {
 
 <style scoped>
 article {
+  align-items: center;
   background-color: aquamarine;
-  width: 48%;
-  margin: 1%;
-  padding: 1%;
+  height: 5rem;
+  justify-content: center;
+  margin-bottom: 1rem;
+  padding: 1%
 }
 
 article h2 {
   text-align: center;
+  line-height: 5rem;
 }
 </style>
