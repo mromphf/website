5fe54bfdf3d0783ba1d2bccacebbeee6e6f30991
Mark Romphf <markromphf@gmail.com>
2025-04-13 08:27:55 -0600

    Migrate the ticket housing into a dedicated 'content' component.

---
+++ src/components/Content.vue
@@ -0,0 +1,53 @@
+<script>
+import Ticket from "@/components/Ticket.vue";
+
+export default {
+  name: "Content",
+  components: {Ticket}
+}
+</script>
+
+<template>
+  <section>
+    <Ticket/>
+    <Ticket/>
+    <Ticket/>
+    <Ticket/>
+    <Ticket/>
+    <Ticket/>
+  </section>
+</template>
+
+<style scoped>
+
+section {
+  border: 1px solid red;
+  column-count: 1;
+  column-gap: 1rem;
+  margin-top: 10vh;
+  max-height: 80vh;
+  overflow-y: auto;
+  padding: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+@media(min-width: 1366px) {
+  section {
+    width: 90%;
+    column-count: 2;
+  }
+}
+
+@media(min-width: 1920px) {
+  section {
+    width: 75%;
+  }
+}
+
+@media(min-width: 2560px){
+  section {
+    width: 60%;
+  }
+}
+</style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,56 +1,19 @@
 <script>
 import Masthead from "@/components/Masthead.vue";
-import Ticket from "@/components/Ticket.vue";
+import Content from "@/components/Content.vue";
 import Menu from "@/components/Menu.vue"
 export default {
   name: "Landing",
-  components: {Ticket, Masthead, Menu}
+  components: {Content, Masthead, Menu}
 }
 </script>
 
 <template>
   <Menu />
   <Masthead />
-  <section id="ticketHousing">
-    <Ticket/>
-    <Ticket/>
-    <Ticket/>
-    <Ticket/>
-    <Ticket/>
-    <Ticket/>
-  </section>
+  <Content />
 </template>
 
 <style scoped>
-#ticketHousing {
-  border: 1px solid red;
-  column-count: 1;
-  column-gap: 1rem;
-  margin-top: 10vh;
-  max-height: 80vh;
-  overflow-y: auto;
-  padding: 1rem;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-@media(min-width: 1366px) {
-  #ticketHousing {
-    width: 90%;
-    column-count: 2;
-  }
-}
-
-@media(min-width: 1920px) {
-  #ticketHousing {
-    width: 75%;
-  }
-}
-
-@media(min-width: 2560px){
-  #ticketHousing {
-    width: 60%;
-  }
-}
 
 </style>
