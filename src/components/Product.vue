c6beed55f9d28fa18945eddcc4e52815f6edcf5c
Mark Romphf <markromphf@gmail.com>
2025-04-17 17:40:30 -0600

    Introduce Product component.

---
+++ src/components/Product.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "Product",
+  props: {
+    title: {
+      type: String,
+      default: "Product"
+    }
+  }
+}
+</script>
+
+<template>
+  <article>
+    <h2>{{ title }}</h2>
+  </article>
+</template>
+
+<style scoped>
+article {
+  align-items: center;
+  background-color: aquamarine;
+  border: 1px solid black;
+  display: grid;
+  height: 20rem;
+  justify-content: center;
+  text-align: center;
+  margin-bottom: 1rem;
+}
+
+
+article:hover {
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.3);
+  color: blue;
+  cursor: pointer;
+}
+
+</style>
