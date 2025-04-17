7aa5aa4f14b765220a6abdb220bb1e22e39e0816
Mark Romphf <markromphf@gmail.com>
2025-04-17 17:41:02 -0600

    Add placeholder products to the maps page.

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -1,16 +1,74 @@
 <script>
+import Product from "@/components/Product.vue";
+
 export default {
-  name: "Maps"
+  name: "Maps",
+  components: {Product},
+  data() {
+    return {
+      products: [
+        {
+          title: "Neverwinter Nights: EE"
+        },
+        {
+          title: "Warcraft II"
+        },
+        {
+          title: "Half-Life 2"
+        },
+        {
+          title: "Warcraft III"
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <template>
-    <p>Coming Soon</p>
+  <section>
+    <Product v-for="product in products" :title="product.title"/>
+  </section>
 </template>
 
 <style scoped>
-p {
-  margin-top: 10vh;
-  text-align: center;
+
+section {
+  border: 1px solid red;
+  column-count: 1;
+  column-gap: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 3vh;
+  max-height: 80vh;
+  overflow-y: auto;
+  padding: 1rem;
 }
+
+Product {
+  break-inside: avoid;
+  display: block;
+  margin-bottom: 1rem;
+}
+
+@media(min-width: 1366px) {
+  section {
+    column-count: 2;
+    margin-top: 10vh;
+    width: 90%;
+  }
+}
+
+@media(min-width: 1920px) {
+  section {
+    width: 75%;
+  }
+}
+
+@media(min-width: 2560px) {
+  section {
+    width: 60%;
+  }
+}
+
 </style>
