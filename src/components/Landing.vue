25d4975a4183e8a4a87eda2428fff9b6f28968f4
Mark Romphf <markromphf@gmail.com>
2025-04-16 20:08:11 -0600

    Add footer with copyright.

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -12,8 +12,15 @@ export default {
   <Menu />
   <Masthead />
   <Content />
+  <footer>
+    Copyright &copy; 2025 Mark Romphf
+  </footer>
 </template>
 
 <style scoped>
-
+footer {
+  background-color: lightcyan;
+  width: 100%;
+  text-align: center;
+}
 </style>
