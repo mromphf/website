f30eadc1109adbe26f00a0a4d78fea5ffdb347cb
Mark Romphf <markromphf@gmail.com>
2025-04-18 08:33:15 -0600

    Rename 'ProductView' to 'Product'

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import About from '@/components/About.vue'
 import Landing from '@/components/Landing.vue'
 import Maps from "@/components/Maps.vue";
-import ProductView from "@/components/ProductView.vue";
+import Product from "@/components/Product.vue";
 
 const routes = [
     { path: '/', component: Landing},
     { path: '/about', component: About },
     { path: '/maps', component: Maps },
-    { name: 'mapview', path: '/maps/:title', component: ProductView },
+    { name: 'mapview', path: '/maps/:title', component: Product },
 ];
 
 const router = createRouter({
