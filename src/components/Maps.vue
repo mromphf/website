fab7c707f2675db916d5d3f961dd375018ec41de
Mark Romphf <markromphf@gmail.com>
2025-04-18 06:32:21 -0600

    Collapse duplicated styles to a common content housing style.

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -26,49 +26,17 @@ export default {
 </script>
 
 <template>
-  <section>
+  <section class="content-housing">
     <Product v-for="product in products" :title="product.title"/>
   </section>
 </template>
 
 <style scoped>
 
-section {
-  border: 1px solid red;
-  column-count: 1;
-  column-gap: 1rem;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 3vh;
-  max-height: 80vh;
-  overflow-y: auto;
-  padding: 1rem;
-}
-
 Product {
   break-inside: avoid;
   display: block;
   margin-bottom: 1rem;
-}
-
-@media(min-width: 1366px) {
-  section {
-    column-count: 2;
-    margin-top: 10vh;
-    width: 90%;
-  }
-}
-
-@media(min-width: 1920px) {
-  section {
-    width: 75%;
-  }
-}
-
-@media(min-width: 2560px) {
-  section {
-    width: 60%;
-  }
 }
 
 </style>
