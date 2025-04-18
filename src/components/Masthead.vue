888e464183d3adf7de5fa71170418e4245a272a9
Mark Romphf <markromphf@gmail.com>
2025-04-18 12:47:40 -0600

    Move images into the src directory.

--- src/components/Masthead.vue
+++ src/components/Masthead.vue
@@ -7,7 +7,7 @@ export default {
 <template>
   <header>
     <section class="left">
-      <img src="/public/Teddy2.jpg" alt="Mike Romeo"/>
+      <img src="/src/assets/Teddy2.jpg" alt="Mike Romeo"/>
     </section>
     <section class="right">
       <h2>Mark</h2>
