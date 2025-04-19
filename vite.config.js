3e104c8ba078543f0490b30f102d356f922c39c8
Mark Romphf <markromphf@gmail.com>
2025-04-19 06:55:34 -0600

    Jigger in the vite-plugin-md dependency, check versions later.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import Markdown from 'vite-plugin-md'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      include: [/\.vue$/, /\.md$/]
+    }),
     vueDevTools(),
+    Markdown()
   ],
   resolve: {
     alias: {
