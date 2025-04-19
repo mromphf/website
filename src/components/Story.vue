3bd9c6627961e580f9a869c32579a43480f60a94
Mark Romphf <markromphf@gmail.com>
2025-04-19 07:32:37 -0600

    Introduce Story component.

---
+++ src/components/Story.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Post from "@/pages/sample-post.md"
+</script>
+
+<template>
+  <section class="content-housing single-column markdown">
+    <Post />
+  </section>
+</template>
+
+<style>
+
+.single-column {
+  column-count: 1;
+}
+
+.markdown p {
+  margin: 1em;
+  text-align: justify;
+}
+
+</style>
