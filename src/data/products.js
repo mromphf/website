855dfdd9da53d11d9933ca7b4e13040a93ffaef2
Mark Romphf <markromphf@gmail.com>
2025-04-19 15:21:20 -0600

    Add 'path' attribute to product data.

--- src/data/products.js
+++ src/data/products.js
@@ -1,22 +1,27 @@
 const products = {
     nwn: {
         id: "nwn",
+        path: "maps",
         title: "Neverwinter Nights: EE"
     },
     war2: {
         id: "war2",
+        path: "maps",
         title: "Warcraft II"
     },
     hl2: {
         id: "hl2",
+        path: "maps",
         title: "Half-Life 2"
     },
     war3: {
         id: "war3",
+        path: "maps",
         title: "Warcraft III"
     },
     fiction: {
         id: "fiction",
+        path: "fiction",
         title: "Fiction",
     }
 }
