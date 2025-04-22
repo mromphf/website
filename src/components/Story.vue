f0e47a9bd76d97d201b45a7af9db3a44287aeb51
Mark Romphf <markromphf@gmail.com>
2025-04-22 15:50:23 -0600

    Render the NotFound component when slug points to nothing.

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {computed, defineAsyncComponent} from "vue";
 import {useRoute} from "vue-router";
+import NotFound from "@/components/NotFound.vue";
 
 const posts = import.meta.glob("@/pages/*.md");
 
@@ -9,7 +10,7 @@ const slug = computed(() => route.params.slug);
 
 const post = computed(() => {
   const loader = posts[`/src/pages/${slug.value}.md`];
-  return loader ? defineAsyncComponent(loader) : null;
+  return loader ? defineAsyncComponent(loader) : NotFound;
 });
 
 </script>
