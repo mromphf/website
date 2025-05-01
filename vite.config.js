3c3299d9ae8d3a03322bf0667d4c7db552298b97
Mark Romphf <markromphf@gmail.com>
2025-05-01 06:07:31 -0600

    Set base?

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/website/',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/]
