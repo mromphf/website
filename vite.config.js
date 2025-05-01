5785b86a494a043d576def69c700ff497b7d0803
Mark Romphf <markromphf@gmail.com>
2025-05-01 11:05:34 -0600

    Change base back to '/' to enable GH Pages custom domain.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/website/',
+  base: '/',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/]
