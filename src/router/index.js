67cf461c47186bd338097ddf42b11075252847e8
Mark Romphf <markromphf@gmail.com>
2025-05-01 07:32:28 -0600

    Switch to hash-based web history.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import About from '@/components/About.vue'
 import Fiction from '@/components/Fiction.vue';
 import Landing from '@/components/Landing.vue'
@@ -18,7 +18,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: routes
 });
 
