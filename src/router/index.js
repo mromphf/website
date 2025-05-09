a422c65a4cbfc807639707f19cd6d160e33e089e
Mark Romphf <markromphf@gmail.com>
2025-05-08 19:06:39 -0600

    Rename 'Story' component to 'Post'

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Fiction from '@/components/Fiction.vue';
 import Landing from '@/components/Landing.vue'
 import Maps from "@/components/Maps.vue";
 import Product from "@/components/Product.vue";
-import Story from "@/components/Story.vue";
+import Post from "@/components/Post.vue";
 import NotFound from "@/components/NotFound.vue";
 
 const routes = [
@@ -13,8 +13,8 @@ const routes = [
     { name: 'maps', path: '/maps', component: Maps , meta: { title: 'Maps & Mods | Mike Romeo' }},
     { name: 'fiction', path: '/fiction', component: Fiction, meta: {title: 'Fiction | Mike Romeo' } },
     { name: 'mapview', path: '/maps/:id', component: Product , meta: {title: 'Maps | Mike Romeo' } },
-    { name: 'story', path: '/fiction/:slug', component: Story, meta: { title: 'Story | Mike Romeo' }},
-    { name: 'post', path: '/blog/:slug', component: Story, meta: { title: 'Blog | Mike Romeo' }},
+    { name: 'story', path: '/fiction/:slug', component: Post, meta: { title: 'tory Post| Mike Romeo' }},
+    { name: 'post', path: '/blog/:slug', component: Post, meta: { title: 'Blog | Mike Romeo' }},
     { name: 'NotFound', path: '/:pathMatch(.*)*', component: NotFound, meta: { title: 'Not Found | Mike Romeo'} },
 ];
 
